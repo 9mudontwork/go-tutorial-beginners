edab2e36905f5851d1536d5d000f4fb6cd81a743
Muhammad <i3acksp4ce@gmail.com>
2022-04-03 18:40:57 +0700

    maps

--- main.go
+++ main.go
@@ -2,18 +2,39 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
-// cannot use shorthand outside of functions
-// scoreTwo := 50
-
 func main() {
-	sayHello("mario")
-	showScore()
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":           4.99,
+		"pie":            7.99,
+		"salad":          6.99,
+		"toffee pudding": 3.55,
 	}
-}
 
-// go run main.go greetings.go
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	aaa := map[int]string{
+		123: "aaa",
+		456: "bbb",
+		789: "ccc",
+	}
+
+	fmt.Println(aaa[123123])
+
+	_, ok := aaa[123]
+	fmt.Printf("ok %#v\n", ok)
+
+	if val, ok := aaa[123]; ok {
+		fmt.Printf("val %#v\n", val)
+	}
+
+	aaa[456] = "zzz"
+	fmt.Println(aaa)
+
+	aaa[789] = "xxx"
+	fmt.Println(aaa)
+}
